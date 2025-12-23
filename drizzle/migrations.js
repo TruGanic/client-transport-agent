2fefa13c6de4c88ad2edb1b2355a536289604db3
asirialwis <asirichathurika900@gmail.com>
2025-12-23 21:46:55 +0530

    db migrations

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_optimal_bloodscream.sql';
 import m0001 from './0001_flat_microbe.sql';
+import m0002 from './0002_lethal_supernaut.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
